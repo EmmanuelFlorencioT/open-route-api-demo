299893110e8f193a87233f1e3821209ec2888635
EmmanuelFlorencioT <emm.ftr2@gmail.com>
2023-11-06 19:47:27 -0600

    Added google maps api

--- lib/map.dart
+++ lib/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MyMap extends StatefulWidget {
   const MyMap({super.key});
@@ -8,8 +9,39 @@ class MyMap extends StatefulWidget {
 }
 
 class _MyMapState extends State<MyMap> {
+  late GoogleMapController mapController;
+
+  late LatLng _center = const LatLng(45.521523, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    //Check for the device size. To scale the height and width of the image.
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Map',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
